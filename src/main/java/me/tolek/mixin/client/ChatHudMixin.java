31eeb95dbb9c72046dfcdd9582c9433343ca7ada
Tolek Rudnicki <tolek@epsi.pl>
2024-06-25 12:45:09 +0200

    fixed a small problem with auto reply not being able to be turned off

--- src/main/java/me/tolek/mixin/client/ChatHudMixin.java
+++ src/main/java/me/tolek/mixin/client/ChatHudMixin.java
@@ -49,19 +49,20 @@ public class ChatHudMixin {
         String message = a.getString();
 
         autoReplies.getAutoReplies().forEach((ar) -> {
-            ar.getKeywords().forEach((String s) -> {
-                if (message.contains(s)) {
-                    int index = (int) Math.round(Math.random() * ar.getReplies().size()) - 1;
-                    String reply = ar.getReplies().get(index == -1 ? 0 : index);
+            if (ar.isTurnedOn())
+                ar.getKeywords().forEach((String s) -> {
+                    if (message.contains(s)) {
+                        int index = (int) Math.round(Math.random() * ar.getReplies().size()) - 1;
+                        String reply = ar.getReplies().get(index == -1 ? 0 : index);
 
-                    if (reply.startsWith("/") && reply != null) {
-                        MinecraftClient.getInstance().player.networkHandler.sendCommand(reply.substring(1));
-                    } else {
-                        MinecraftClient.getInstance().player.networkHandler.sendChatMessage(reply);
+                        if (reply.startsWith("/") && reply != null) {
+                            MinecraftClient.getInstance().player.networkHandler.sendCommand(reply.substring(1));
+                        } else {
+                            MinecraftClient.getInstance().player.networkHandler.sendChatMessage(reply);
+                        }
                     }
-                }
 
-            });
+                });
         });
     }
 
