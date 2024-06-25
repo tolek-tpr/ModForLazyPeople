6a0ffe50441526ddf47e6ab219a5f41988bc07e9
Tolek Rudnicki <tolek@epsi.pl>
2024-06-25 12:07:54 +0200

    Updated version to 2.5.0

--- src/main/java/me/tolek/util/InstancedValues.java
+++ src/main/java/me/tolek/util/InstancedValues.java
@@ -14,7 +14,7 @@ public class InstancedValues {
     public boolean shownWelcomeScreen = false;
     public boolean hasLoaded = false;
     public boolean updateAvailable = false;
-    public String version = "v2.4.5";
+    public String version = "v2.5.0";
     public long timeSinceLastInputInMils = 0;
     public String githubUrl = "github.com/tolek-tpr/ModForLazyPeople/releases";
     public boolean shownUpdateScreen = false;
