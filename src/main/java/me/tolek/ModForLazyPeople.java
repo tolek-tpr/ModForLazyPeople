001fdc5f8dfa7834a3a42c15fe2acd92891cd3f6
tolek-tpr <tolek@epsi.pl>
2024-10-09 18:20:41 +0200

    Removed unused library

--- src/main/java/me/tolek/ModForLazyPeople.java
+++ src/main/java/me/tolek/ModForLazyPeople.java
@@ -1,6 +1,5 @@
 package me.tolek;
 
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.tolek.event.EventImpl;
 import me.tolek.events.AfkDetector;
 import me.tolek.files.MflpConfigImpl;
@@ -17,14 +16,12 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 
-import static net.minecraft.server.command.CommandManager.*;
-
 public class ModForLazyPeople implements ModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("modforlazypeople");
 	public static final MflpServerConnection serverConnection = new MflpServerConnection();
 
-	private ArrayList<EventImpl> events = new ArrayList<>();
+	private final ArrayList<EventImpl> events = new ArrayList<>();
 
 	@Override
 	public void onInitialize() {
