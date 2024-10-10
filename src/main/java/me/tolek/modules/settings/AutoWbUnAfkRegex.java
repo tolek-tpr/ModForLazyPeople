17557c71451827cb81c9459a4eb7a4294516cc76
BeefyAndTheDucks <simonsej123gamerminecraft@gmail.com>
2024-10-10 21:43:25 +0200

    Replace defaults in settings to use %u syntax

--- src/main/java/me/tolek/modules/settings/AutoWbUnAfkRegex.java
+++ src/main/java/me/tolek/modules/settings/AutoWbUnAfkRegex.java
@@ -6,8 +6,8 @@ import me.tolek.util.RegexUtil;
 public class AutoWbUnAfkRegex extends StringSetting {
 
     public AutoWbUnAfkRegex() {
-        super("Un-AFK RegEx", "^[a-zA-Z0-9_]{3,16} is no longer AFK\\.$", "The RegEx to use to match if a message contains the user has returned from afk text.");
-        this.setState("^[a-zA-Z0-9_]{3,16} is no longer AFK\\.$");
+        super("Un-AFK RegEx", "^%u is no longer AFK\\.$", "The RegEx to use to match if a message contains the user has returned from afk text.");
+        this.setState("^%u is no longer AFK\\.$");
         this.render = false;
     }
 
