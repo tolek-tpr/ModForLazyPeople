78beb27c1f921567f3c22fab382ffd8f324363fa
BeefyAndTheDucks <72296886+BeefyAndTheDucks@users.noreply.github.com>
2024-10-15 21:49:52 +0200

    ok so im dumb but its fixed now

--- src/main/java/me/tolek/commands/client/ClientMlfpCommand.java
+++ src/main/java/me/tolek/commands/client/ClientMlfpCommand.java
@@ -20,28 +20,28 @@ public class ClientMlfpCommand implements ClientModInitializer {
     public void onInitializeClient() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             dispatcher.register(literal("party")
-                    .then(literal("invite").then(argument("user", StringArgumentType.word())))
+                    .then(literal("invite").then(argument("user", StringArgumentType.word()))
                     .executes(context -> {
                         String user = StringArgumentType.getString(context, "user");
                         ModForLazyPeople.LOGGER.info("INVITE {}", user);
                         context.getSource().sendFeedback(Text.literal("Player has been invited. (or not because it isn't implemented yet lmao)")); // TODO: Text.translatable
                         return 1;
-                    })
+                    }))
 
-                    .then(literal("accept"))
+                    .then(literal("accept")
                     .executes(context -> {
                         ModForLazyPeople.LOGGER.info("ACCEPT");
                         context.getSource().sendFeedback(Text.literal("Invite Accepted! (or not because it isn't implemented yet lmao)")); // TODO: Text.translatable
                         return 1;
-                    })
+                    }))
 
-                    .then(literal("send").then(argument("message", StringArgumentType.greedyString())))
+                    .then(literal("send").then(argument("message", StringArgumentType.greedyString()))
                     .executes(context -> {
                         String message = StringArgumentType.getString(context, "message");
                         ModForLazyPeople.LOGGER.info("SEND {}", message);
                         context.getSource().sendFeedback(Text.literal("You: ").append(message).formatted(Formatting.ITALIC, Formatting.GRAY)); // TODO: Text.translatable
                         return 1;
-                    }));
+                    })));
         });
     }
 }
