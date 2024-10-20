df88edf4a3d690c35af4fd13ef83bac5cf240aa0
BeefyAndTheDucks <72296886+BeefyAndTheDucks@users.noreply.github.com>
2024-10-20 20:01:28 +0200

    /mflpnet status command

--- src/main/java/me/tolek/commands/client/MflpNetCommand.java
+++ src/main/java/me/tolek/commands/client/MflpNetCommand.java
@@ -17,12 +17,20 @@ public class MflpNetCommand implements ClientModInitializer {
                     .then(literal("reconnect").executes(context -> {
                         WebSocketServerHandler.getInstance().reconnect();
                         if (WebSocketServerHandler.getInstance().endpoint == null) {
-                            context.getSource().sendFeedback(Text.literal("MFLP > Failed to reconnect!").formatted(Formatting.RED));
+                            context.getSource().sendFeedback(Text.translatable("mflp.net.reconnect.failure").formatted(Formatting.RED));
                         } else {
-                            context.getSource().sendFeedback(Text.literal("MFLP > Connection succeeded!").formatted(Formatting.GREEN));
+                            context.getSource().sendFeedback(Text.translatable("mflp.net.reconnect.success").formatted(Formatting.GREEN));
                         }
                         return 1;
                     }))
+                    .then(literal("status")).executes(context -> {
+                        if (WebSocketServerHandler.getInstance().endpoint == null) {
+                            context.getSource().sendFeedback(Text.translatable("mflp.net.status.notConnected").formatted(Formatting.RED));
+                        } else {
+                            context.getSource().sendFeedback(Text.translatable("mflp.net.status.connected").formatted(Formatting.GREEN));
+                        }
+                        return 1;
+                    })
             );
         });
     }
