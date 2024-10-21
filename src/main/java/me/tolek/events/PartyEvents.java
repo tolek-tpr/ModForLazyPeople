20b5a4742e7846216fc01a588b7d7c287f054fc4
tolek-tpr <tolek@epsi.pl>
2024-10-21 22:59:40 +0200

    Changed a few messages and formatting

--- src/main/java/me/tolek/events/PartyEvents.java
+++ src/main/java/me/tolek/events/PartyEvents.java
@@ -30,56 +30,57 @@ public class PartyEvents extends EventImpl implements PartyListener {
     @Override
     public void onMessage(String message, String author) {
         // Terrifying. Minecraft hates me and this was the only way it works. MutableText.formatting didn't work properly.
-        MutableText authorText = Text.literal(author).styled(style -> style.withBold(true));
+        MutableText partyText = Text.literal("MFLP Party > ").formatted(Formatting.BLUE);
+        MutableText authorText = Text.literal(author).styled(style -> { style.withBold(true);  style.withColor(Formatting.WHITE); return style; });
         MutableText colonText = Text.literal(": ").styled(style -> style.withBold(false)); // Reset the bold for colon
         MutableText messageText = Text.literal(message).styled(style -> style.withItalic(true).withColor(Formatting.GRAY).withBold(false));
 
-        MutableText combinedText = authorText.append(colonText).append(messageText);
+        MutableText combinedText = partyText.append(authorText).append(colonText).append(messageText);
 
         sendFeedback(combinedText, !author.equals(MinecraftClient.getInstance().getSession().getUsername()));
     }
 
     @Override
     public void onPlayerInvited(String playerUsername) {
-        sendFeedback(Text.translatable("mflp.party.playerInvited", playerUsername).formatted(Formatting.ITALIC, Formatting.GRAY));
+        sendFeedback(Text.translatable("mflp.party.playerInvited", playerUsername).formatted(Formatting.ITALIC, Formatting.GREEN));
     }
 
     @Override
     public void onClientInvited(String partyOwnerUsername) {
         ToastUtil.showToast(Text.translatable("mflp.party.clientInvited.title", partyOwnerUsername), Text.translatable("mflp.party.clientInvited.description", partyOwnerUsername));
-        sendFeedback(Text.translatable("mflp.party.clientInvited.title", partyOwnerUsername).formatted(Formatting.ITALIC, Formatting.GRAY));
+        sendFeedback(Text.translatable("mflp.party.clientInvited.title", partyOwnerUsername).formatted(Formatting.ITALIC, Formatting.GREEN));
     }
 
     @Override
     public void onClientRemoved() {
         ToastUtil.showToast(Text.translatable("mflp.party.clientRemoved.title"), Text.empty());
-        sendFeedback(Text.translatable("mflp.party.clientRemoved.title").formatted(Formatting.ITALIC, Formatting.GRAY));
+        sendFeedback(Text.translatable("mflp.party.clientRemoved.title").formatted(Formatting.ITALIC, Formatting.RED));
     }
 
     @Override
     public void onClientLeft() {
-        sendFeedback(Text.translatable("mflp.party.clientLeft").formatted(Formatting.ITALIC, Formatting.GRAY));
+        sendFeedback(Text.translatable("mflp.party.clientLeft").formatted(Formatting.ITALIC, Formatting.RED));
     }
 
     @Override
     public void onPlayerLeft(String playerUsername) {
-        sendFeedback(Text.translatable("mflp.party.playerLeft", playerUsername).formatted(Formatting.ITALIC, Formatting.GRAY));
+        sendFeedback(Text.translatable("mflp.party.playerLeft", playerUsername).formatted(Formatting.ITALIC, Formatting.RED));
     }
 
     @Override
     public void onPlayerRemoved(String playerUsername) {
-        sendFeedback(Text.translatable("mflp.party.playerRemoved", playerUsername).formatted(Formatting.ITALIC, Formatting.GRAY));
+        sendFeedback(Text.translatable("mflp.party.playerRemoved", playerUsername).formatted(Formatting.ITALIC, Formatting.RED));
     }
 
     @Override
     public void onPlayerJoined(String playerUsername) {
-        sendFeedback(Text.translatable("mflp.party.playerJoined", playerUsername).formatted(Formatting.ITALIC, Formatting.GRAY));
+        sendFeedback(Text.translatable("mflp.party.playerJoined", playerUsername).formatted(Formatting.ITALIC, Formatting.GREEN));
     }
 
     @Override
     public void onPartyInviteFailed(String username) {
         ToastUtil.showToast(Text.translatable("mflp.party.failedToInvite.title", username), Text.translatable("mflp.party.failedToInvite.description"));
-        sendFeedback(Text.translatable("mflp.party.failedToInvite.title", username).formatted(Formatting.ITALIC, Formatting.GRAY));
+        sendFeedback(Text.translatable("mflp.party.failedToInvite.title", username).formatted(Formatting.ITALIC, Formatting.RED));
     }
 
     @Override
@@ -97,17 +98,17 @@ public class PartyEvents extends EventImpl implements PartyListener {
 
     @Override
     public void onPlayerDemoted(String player) {
-        sendFeedback(Text.translatable("mflp.party.playerDemoted", player).formatted(Formatting.ITALIC, Formatting.GRAY));
+        sendFeedback(Text.translatable("mflp.party.playerDemoted", player).formatted(Formatting.ITALIC, Formatting.RED));
     }
 
     @Override
     public void onPlayerPromoted(String player) {
-        sendFeedback(Text.translatable("mflp.party.playerPromoted", player).formatted(Formatting.ITALIC, Formatting.GRAY));
+        sendFeedback(Text.translatable("mflp.party.playerPromoted", player).formatted(Formatting.ITALIC, Formatting.GREEN));
     }
 
     @Override
     public void onPlayerDeclinedInvite(String player) {
-        sendFeedback(Text.translatable("mflp.party.playerDeclined", player).formatted(Formatting.ITALIC, Formatting.GRAY));
+        sendFeedback(Text.translatable("mflp.party.playerDeclined", player).formatted(Formatting.ITALIC, Formatting.RED));
     }
 
     private static void sendFeedback(Text message, boolean playSound) {
