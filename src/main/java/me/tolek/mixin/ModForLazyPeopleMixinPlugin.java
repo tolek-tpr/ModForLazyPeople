42a2028e6814a9bcbe386694e1b598e0c013e343
BeefyAndTheDucks <72296886+BeefyAndTheDucks@users.noreply.github.com>
2025-12-31 14:11:02 +0100

    Improve Mixin dependency checking

--- src/main/java/me/tolek/mixin/ModForLazyPeopleMixinPlugin.java
+++ src/main/java/me/tolek/mixin/ModForLazyPeopleMixinPlugin.java
@@ -1,14 +1,23 @@
 package me.tolek.mixin;
 
+import me.tolek.mixin.client.Tweakeroo_CameraEntityMixin;
 import me.tolek.util.MiscUtils;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
 public class ModForLazyPeopleMixinPlugin implements IMixinConfigPlugin {
+
+    @SuppressWarnings("ReferenceToMixin")
+    private final HashMap<String, String[]> mixinDependencyMap = new HashMap<>() {
+        {
+            put(Tweakeroo_CameraEntityMixin.class.getName(), new String[] { "tweakeroo" });
+        }
+    };
 
     @Override
     public void onLoad(String mixinPackage) {
@@ -22,13 +31,19 @@ public class ModForLazyPeopleMixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        boolean isTweakerooLoaded = MiscUtils.isModLoaded("tweakeroo");
-
-        // TODO: this is hella messy, but it works for now
-        if (mixinClassName.equals("me.tolek.mixin.client.Tweakeroo_CameraEntityMixin")) {
-            return isTweakerooLoaded; // Only apply if tweakeroo is loaded
+        // Check if it has dependencies
+        if (mixinDependencyMap.containsKey(mixinClassName)) {
+            // It has dependencies: go through them
+            for (String dependency : mixinDependencyMap.get(mixinClassName)) {
+                // Check if the dependency is loaded
+                if (!MiscUtils.isModLoaded(dependency))
+                    // It's not, we cannot apply this mixin.
+                    return false;
+            }
         }
-        return true; // Apply other mixins
+
+        // All dependencies (if any) are loaded, we can apply it.
+        return true;
     }
 
     @Override
